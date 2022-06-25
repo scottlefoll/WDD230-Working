ee7a594f3bff18dccc4e01f18738a57faeb164b1
scottlefoll <51521131+scottlefoll@users.noreply.github.com>
2022-06-25 12:51:08 -0400

    update

--- weather/js/script.js
+++ weather/js/script.js
@@ -6,18 +6,30 @@
 // let text = document.lastModified;
 // document.getElementById("mod-date").innerHTML = text;
 
+
+// Current Date at bottom of page
 // let dt = new Date();
 // const currentDate = new Date();
 // const options = { weekday: 'long', day: 'numeric', month: 'short', year: 'numeric'};
 // document.getElementById('date-time').innerHTML=currentDate.toLocaleDateString('en-uk', options);
 
 
-// function myFunction() {
-//   var x = document.getElementById("myLinks");
-//   if (x.style.display === "block") {
-//     x.style.display = "none";
-//   } else {
-//     x.style.display = "block";
-//   }
-// }
+// Get Date
+const d = new Date();
+// Get Day of week as int; Friday = 5
+let day = d.getDay()
+const para = document.createElement("p");
+para.innerHTML = "Saturday: Preston Pancakes in the Park!<br> 9:00 am Saturday, 6/25, in Preston Community Park.";
 
+// Append to the top banner element:
+document.getElementById("topDIV").appendChild(para);
+
+//If day = 5 show ad, otherwise hide it.
+if (day === 6 || day === 5) {
+  document.getElementById("topDIV").style.display = "block";
+  } else {
+    document.getElementById("topDIV").style.display = "none";
+  }
+document.getElementById("topDIV").style.backgroundColor="yellow";
+document.getElementById("topDIV").style.fontWeight="bold";
+document.getElementById("topDIV").style.fontSize=".95rem";
