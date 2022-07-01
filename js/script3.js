f6b0fe802d29c1b0ea9385424338ec2f8de7a7d4
scottlefoll <51521131+scottlefoll@users.noreply.github.com>
2022-07-01 12:06:25 -0400

    update

--- js/script3.js
+++
@@ -1,7 +0,0 @@
-// This is your site JavaScript code - you can add interactivity and carry out processing
-
-let year_text = new Date().getFullYear();
-document.getElementById("c_year").innerHTML = year_text;
-
-let text = document.lastModified;
-document.getElementById("mod-date").innerHTML = text;
