632c4ee9d51b1f18a24a9b30de7bd215c93b4b7f
Scott LeFoll <51521131+scottlefoll@users.noreply.github.com>
2022-07-19 15:11:40 -0500

    update

--- week07-scoots/js/rentals.js
+++ week07-scoots/js/rentals.js
@@ -6,3 +6,9 @@ alert(myData[1].type);
 alert(myData[1].max);
 
 
+document.getElementById("model1").innerHTML = myData[0].type;
+document.getElementById("model2").innerHTML = myData[1].type;
+document.getElementById("model3").innerHTML = myData[2].type;
+document.getElementById("model4").innerHTML = myData[3].type;
+document.getElementById("model5").innerHTML = myData[4].type;
+document.getElementById("model6").innerHTML = myData[5].type;
